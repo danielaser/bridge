377d3c8c3cd24d642edba8cd0eefd63b991751d3
danielaser <daniela.serrano.dev@gmail.com>
2024-12-21 18:37:01 -0500

    feat(develop): add user class

---
+++ src/main/java/org/example/user/User.java
@@ -0,0 +1,20 @@
+package org.example.user;
+
+public class User {
+    private String name;
+
+    public User(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public User() {
+    }
+}
